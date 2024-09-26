f80480147f84b02cc3c16889323bb9a65e5ecb19
Henrich-B <127859914+Henrich-B@users.noreply.github.com>
2024-09-26 17:41:59 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,24 +6,20 @@ const employeeRoutes = require('./routes/employeeRoutes');
 const productRoutes = require('./routes/productRoutes');
 const promotionRoutes = require('./routes/promotionRoutes');
 const authRoutes = require('./routes/authRoutes');
-require('dotenv').config(); // Para usar variáveis de ambiente
+require('dotenv').config(); 
 
 const app = express();
 
-// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 
-// Rotas de Autenticação
 app.use('/api/auth', authRoutes);
 
-// Rotas Protegidas
 app.use('/api/clients', clientRoutes);
 app.use('/api/employees', employeeRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/promotions', promotionRoutes);
 
-// Conectar ao MongoDB
 mongoose.connect('mongodb+srv://izuku1208:iEE2tzoC9uRRlv6p@clientes.weuoj.mongodb.net/Supermarket', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(3000, () => {
