d074162538b790d18d0fc6faf9a44d4195dd2f68
Henrich-B <127859914+Henrich-B@users.noreply.github.com>
2024-09-26 22:39:21 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const clientRoutes = require('./routes/clientRoutes');
-const employeeRoutes = require('./routes/employeeRoutes');
-const productRoutes = require('./routes/productRoutes');
-const promotionRoutes = require('./routes/promotionRoutes');
-const authRoutes = require('./routes/authRoutes');
+const employeeRoutes = require('./src/routes/employeeRoutes');
+const productRoutes = require('./src/routes/productRoutes');
+const promotionRoutes = require('./src/routes/promotionRoutes');
+const authRoutes = require('./src/routes/authRoutes');
+const userRoutes = require('./src/routes/userRoutes')
 require('dotenv').config(); 
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 
+// Conectar ao MongoDB
+
+
+// Rotas
 app.use('/api/auth', authRoutes);
-
-app.use('/api/clients', clientRoutes);
-app.use('/api/employees', employeeRoutes);
+app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/employees', employeeRoutes);
 app.use('/api/promotions', promotionRoutes);
-
-mongoose.connect('mongodb+srv://izuku1208:iEE2tzoC9uRRlv6p@clientes.weuoj.mongodb.net/Supermarket', { useNewUrlParser: true, useUnifiedTopology: true });
+app.get('/api/protected', authMiddleware, (req, res) => {
+  res.send('Esta é uma rota protegida');
+});
+mongoose.connect('mongodb://localhost:27017/Supermarket', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
