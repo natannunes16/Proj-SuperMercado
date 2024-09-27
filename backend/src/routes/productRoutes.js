44b025ff9900b82463b9883d3a5a00e8038ab07e
Henrich-B <127859914+Henrich-B@users.noreply.github.com>
2024-09-26 22:37:44 -0300

    Update productRoutes.js

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { createProduct } = require('../controllers/productController');
-const { protect } = require('../middlewares/authMiddleware');
+const { createPromotion, getPromotion, updatePromotion, deletePromotion } = require('../controllers/promotionController');
+const { verifyToken } = require('../middlewares/authMiddleware');
+
 const router = express.Router();
 
-router.post('/', protect, createProduct);
+router.post('/', verifyToken, createPromotion);
+router.get('/:id', verifyToken, getPromotion);
+router.put('/:id', verifyToken, updatePromotion);
+router.delete('/:id', verifyToken, deletePromotion);
 
 module.exports = router;
