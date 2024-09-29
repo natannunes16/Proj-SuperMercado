47f91931cb9df583afeed1d4570c6075744bba28
Henrich-B <127859914+Henrich-B@users.noreply.github.com>
2024-09-29 11:53:28 -0300

    Update Product.js

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    nome: { type: String, required: true },
-    categoria: { type: String, required: true },
-    preco: { type: Number, required: true },
-    precoPromocional: { type: Number, default: null },
-    descricao: { type: String },
-    dataValidade: { type: Date, required: true }
+  nome: { type: String, required: true },
+  precoAtual: { type: Number, required: true },
+  precoPromocional: { type: Number, default: null },
+  tipo: { type: String, required: true },
+  descricao: { type: String },
+  validade: { type: Date, required: true }
 });
 
-const Product = mongoose.model('Product', productSchema);
-module.exports = Product;
+module.exports = mongoose.model('Produto', productSchema);
