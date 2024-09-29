26046e1f23634cd86d34ecfe9a82554742a7e3b9
Henrich-B <127859914+Henrich-B@users.noreply.github.com>
2024-09-29 11:54:43 -0300

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,19 +2,18 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    nome: { type: String, required: true },
-    cpf: { type: String, required: true, unique: true },
-    senha: { type: String, required: true }
+  nome: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  cpf: { type: String, required: true, unique: true },
+  senha: { type: String, required: true },
+  dataCadastro: { type: Date, default: Date.now }
 });
 
-// Middleware para hash da senha antes de salvar
+// Criptografar senha antes de salvar
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('senha')) {
-        next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.senha = await bcrypt.hash(this.senha, salt);
+  if (!this.isModified('senha')) return next();
+  this.senha = await bcrypt.hash(this.senha, 10);
+  next();
 });
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+module.exports = mongoose.model('Usuario', userSchema);
