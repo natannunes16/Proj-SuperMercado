9a662a6d6675592e8def73c6208fd58106d69b62
Henrich-B <127859914+Henrich-B@users.noreply.github.com>
2024-09-29 11:53:44 -0300

    Update Promotion.js

--- backend/src/models/Promotion.js
+++ backend/src/models/Promotion.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const promotionSchema = new mongoose.Schema({
-    produtoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-    descontoPercentual: { type: Number, required: true },
-    dataInicio: { type: Date, required: true },
-    dataFim: { type: Date, required: true }
+  produto: { type: mongoose.Schema.Types.ObjectId, ref: 'Produto', required: true },
+  desconto: { type: Number, required: true },
+  dataInicio: { type: Date, required: true },
+  dataFim: { type: Date, required: true }
 });
 
-const Promotion = mongoose.model('Promotion', promotionSchema);
-module.exports = Promotion;
+module.exports = mongoose.model('Promocao', promotionSchema);
