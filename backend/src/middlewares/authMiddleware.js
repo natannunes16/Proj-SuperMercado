d0ab003f18d262fc93722ddb2c1799aa1ea4681c
Henrich-B <127859914+Henrich-B@users.noreply.github.com>
2024-10-01 09:15:58 -0300

    Update authMiddleware.js

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,19 +1,29 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-  const token = req.cookies.token || '';
+  
+  const token = req.cookies.token || req.headers.authorization?.split(' ')[1] || '';
 
   if (!token) {
     return res.status(401).json({ message: 'Autenticação necessária' });
   }
 
   try {
+   
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(403).json({ message: 'Token inválido' });
+    if (error.name === 'TokenExpiredError') {
+      return res.status(403).json({ message: 'Token expirado' });
+    } else if (error.name === 'JsonWebTokenError') {
+      return res.status(403).json({ message: 'Token inválido' });
+    } else {
+      return res.status(403).json({ message: 'Erro na verificação do token' });
+    }
   }
 };
+
 
 module.exports = authMiddleware;
